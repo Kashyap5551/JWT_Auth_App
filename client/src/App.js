ee8df2301c56b5edc1747d5be2a9db40fcb7c1ac
Kashyap5551 <kash5551@my.yorku.ca>
2022-01-19 09:29:16 -0500

    react router change

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,31 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from "react";
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 
 //components
-import Dashboard from './components/Dashboard';
-import Login from './components/Login';
-import Register from './components/Register';
-
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-
   const [isAuthenticated, setAuthenticated] = useState(false);
 
-  const setAuth = boolean => {
+  const setAuth = (boolean) => {
     setAuthenticated(boolean);
-  }
+  };
   async function isAuth() {
     try {
-       const res = await fetch("http://localhost:5001/auth/verify", {
+      const res = await fetch("http://localhost:5001/auth/verify", {
         method: "POST",
-        headers: { jwt_token: localStorage.token }
-       });
-      
-        const parseRes = await res.json();
+        headers: { jwt_token: localStorage.token },
+      });
+
+      const parseRes = await res.json();
 
       parseRes === true ? setAuthenticated(true) : setAuthenticated(false);
     } catch (err) {
@@ -35,17 +33,58 @@ function App() {
     }
   }
   useEffect(() => {
-    isAuth()
-  }, [])
+    isAuth();
+  }, []);
 
   return (
     <Fragment>
       <Router>
         <div>
           <Routes>
-            <Route exact path="/login" element={ !isAuthenticated ? (<Login  setAuth={ setAuth }/>) : (<Navigate replace to="/dashboard" />) } />
-            <Route exact path="/register" element= { !isAuthenticated ? (<Register  setAuth={ setAuth } />) : (<Navigate replace to ="/login"/>) } />
-            <Route exact path="/dashboard" element= { isAuthenticated ? (<Dashboard  setAuth={ setAuth } />) : (<Navigate replace to="/login" />) } />
+            <Route
+              exact
+              path="/"
+              element={
+                !isAuthenticated ? (
+                  <Login setAuth={setAuth} />
+                ) : (
+                  <Navigate replace to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/login"
+              element={
+                !isAuthenticated ? (
+                  <Login setAuth={setAuth} />
+                ) : (
+                  <Navigate replace to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              element={
+                !isAuthenticated ? (
+                  <Register setAuth={setAuth} />
+                ) : (
+                  <Navigate replace to="/login" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              element={
+                isAuthenticated ? (
+                  <Dashboard setAuth={setAuth} />
+                ) : (
+                  <Navigate replace to="/login" />
+                )
+              }
+            />
           </Routes>
         </div>
       </Router>
